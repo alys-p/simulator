717013bef55f89566e423cc780297fa3314330b8
ap-carrot <arianerolong@gmail.com>
2021-05-25 00:54:00 +0200

    ive started, but all ive done is make the grille and a color code for the cells

--- affichage.py
+++ affichage.py
@@ -1,1 +1,17 @@
-x = 10
+def afficher_status(status):
+    code = {'morte': 1, 'vivante': 2, 'immune': 4}
+    couleur_stat = 30 + code[status]
+    return("\033[{}m\033[{}m{}\033[0m".format(couleur_stat, couleur_stat, " "))
+
+
+def creation_grille(n):
+    for j in range(n[1]):
+        for i in range(n[2]):
+            L = []
+            L.append(i)
+        ind = []
+        ind.append(L)
+    return(ind)
+
+
+
