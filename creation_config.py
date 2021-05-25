b04d4c72b306b105900d71b95311639c8fc5acef
alys-p <alysp@live.fr>
2021-05-25 18:47:28 +0200

    i commented that file too :)

--- creation_config.py
+++ creation_config.py
@@ -2,12 +2,19 @@
 
 from parametres import *
 import random
-import pprint
+import pprint # juste pour un print plus jolie et repérer facilement les erreurs potentielles
 
 # CREATION DE LA GRILLE DE BASE
 
-def configuration_initial (v, n):
-    G = []
+def configuration_initial (v, n, p):
+    """
+    :param n: dimension de la grille (tuple) pour determiner l'espace et donc le nombre de cellules vivante possible.
+    :param v: pourcentage de cellules vivante par rapport au maximum d'espace (n[0]*n[1])
+    :param p: pourcentage de cellules contaminé dans les cellules vivantes
+    :return: la grille G complète et prète pour démarer la simulation avec un tuple de position et un dico pour chaque cellule
+    """
+
+    G = []  # création de la grille et remplissage progressif
     print(v)
     for i in range(0, n[0]):
         for j in range(0, n[1]):
@@ -17,16 +24,16 @@ def configuration_initial (v, n):
             dico_cell["valeur"] = 2
             print(C)
             G.append(C)
-    m = 0
+    m = 0       # variable d'incrémentation parceque c'est un while donc pas il y a pas d'incrémentation de 1 automatique
     L = []
     while m <= ((n[0]*n[1]*(v/100))-1):
         valeur_random = random.randint(0, n[0]*n[1]-1)
-        if G[valeur_random][1]["etat"] == "morte":
-            G[valeur_random][1]["etat"] = "vivante"
+        if G[valeur_random][1]["etat"] == "morte":      # verification que la cellue est bien morte pour en avoir le bon nombre
+            G[valeur_random][1]["etat"] = "vivante"     # de vivante and pa un cellule attribué vivante 2 fois ou plus
             L.append(valeur_random)
             m = m +1
     print(L)
-    o = 0
+    o = 0       # variable d'incrémentation parceque c'est un while donc pas il y a pas d'incrémentation de 1 automatique
     while o <= ((m*(p/100))-1):
         print(o)
         valeur_random_2 = random.choice(L)
@@ -34,5 +41,5 @@ def configuration_initial (v, n):
         o = o +1
     return G
 
-pprint.pprint(configuration_initial(v, n))
+pprint.pprint(configuration_initial(v, n, p))
 
