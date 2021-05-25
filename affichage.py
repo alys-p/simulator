bbfcdfc9e2948ee9e0d827d8e83cfb01c4c60f06
ap-carrot <arianerolong@gmail.com>
2021-05-25 20:23:31 +0200

    i fixed some stuff, also status is like dead, alive, contaminated etc.

--- affichage.py
+++ affichage.py
@@ -1,19 +1,15 @@
-def afficher_status(status): # what is a status?
-    code = {'morte': 1, 'vivante': 2, 'immune': 4}
+def definir_couleur(status):
+    code = {'morte': 1, 'vivante': 2, 'immune': 4, 'contaminee': 5}
     couleur_stat = 30 + code[status]
     return("\033[{}m\033[{}m{}\033[0m".format(couleur_stat, couleur_stat, " "))
 
+def definir_status(G):
+    for i in range(len(G)):
+        for j in range(len(G[i])):
+            if G[i][j][2]["etat" == "morte"
 
-def creation_grille(n):
-    for j in range(n[1]):
-        for i in range(n[2]):   # n[2] doesn't exist i think you mean n[0] and n[1] also i thing this function is useless, i did
-            L = []              # this already in config initial but correct me if im wrong, im a bit lost rn.
-            L.append("")
-        ind = []
-        ind.append(L)
-    return(ind)
 
-def definir_status():
+
 
 
 
