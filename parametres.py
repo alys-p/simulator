b55ac76e70fd80ad8efc218354f93aa6fcd6c103
alys-p <alysp@live.fr>
2021-05-25 18:31:59 +0200

    I added comments that way you can understand better what i did :) I also modified the way of setting the size of the grille that why the carre is treated as any rectangle. I don't think it's necessary to make it a special case

--- parametres.py
+++ parametres.py
@@ -1,25 +1,20 @@
 # PARAMETRES
 
-def carre_rectangle (n):
-    if n == "carre":
-        c = int(input("valeur du coté: "))
-        n = (c, c)
-    elif n == "rectangle":
-        h = int(input("hauteur de la grille: "))
-        k = int(input("longueur de la grille: "))
-        n = (h, k)
-    return(n)
+longueur = int(input("Entrer la longueur de la grille: "))
+largeur = int(input("Entrer la largeur de la grille: "))
+n = (longueur, largeur)     # tuple des dimensions de la grille
 
-n = input("entrer 'carre' ou 'rectangle' en fonction de la forme de grille désirée: ")
-while n != "carre" and n != "rectangle":
-    n = input("entrer 'carre' ou 'rectangle' en fonction de la forme de grille désirée: ")
-
-n = carre_rectangle(n)
 print(n)
 
-v = int(input("densité de cellule vivante initialement (%): "))
-p = int(input("densité de cellule vivante initialement (%): "))
-R = int(input("nombre d'individue contaminé par chaque malade: "))
+v = int(input("densité de cellule vivante initialement (%): "))     # on concidère qu'une cellule fait 1 par 1 et donc il y a dans une
+                                                                    # cellule un maximum de n[0]*n[1] cellule. Ici on demenade juste
+                                                                    # le pourcentage du maximum de cellule possible est vivante.
+
+p = int(input("densité de cellule comtaminées initialement (%): "))     # même idée qu'au dessus mais ici il s'agit du pourcentatage des
+                                                                    # cellules vivante qui sont contaminé à l'origine.
+
+R = int(input("nombre d'individue contaminé par chaque malade: "))      # nombre d'individue contaminées dans le vosinage de la cellule
+                                                                        # soit en général dans un visinage de 8
 t_maladie = int(input("durée de la maladie (jours): "))
 taux_mortalite = float(input("probabilité de mourir de la maladie (%): "))
 
