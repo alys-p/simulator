f54dc0ef7cac2f4ac863fbd2acbf4ae3043aa6de
alys-p <alysp@live.fr>
2021-05-28 22:52:50 +0200

    i started a somewhat of commenting

--- simulation.py
+++ simulation.py
@@ -5,7 +5,7 @@ from mise_a_jour import *
 
 G = configuration_initial(v, n, p)
 
-for i in range(nb_tours):
+for i in range(nb_tours):       # boucle d'éxecution de la simulation en fonction du nombre de tour voulue.
     print()
     affichage.afficher_grille(G)
     print()
