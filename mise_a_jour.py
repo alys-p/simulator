4192b745b6dd8329c7a94be33d62924bc25e0eb0
alys-p <alysp@live.fr>
2021-05-28 20:02:43 +0200

    it works!!!!!

--- mise_a_jour.py
+++ mise_a_jour.py
@@ -2,6 +2,7 @@
 
 from parametres import *
 from creation_config import *
+import random
 
 def parcours_cell (G):
     for i in range(len(G)):
@@ -19,10 +20,23 @@ def transition (G, C):
                     if G[m][0] == (k, l):
                         if G[m][1]["etat"] == "contaminee":
                             nb_contaminee = nb_contaminee +1
-                            print(G[m])
+
+        if nb_contaminee > R:
+            G[C][1]["etat"] = "contaminee"
+
+    if G[C][1]["valeur"] == t_maladie:
+        valeur_random = random.randint(0,100)
+        if valeur_random > taux_mortalite:
+            G[C][1]["etat"] = "immunisee"
+        else:
+            G[C][1]["etat"] = "decedee"
+            G[C][1]["valeur"] = 0
+
+
     if G[C][1]["etat"] == "contaminee":
         G[C][1]["valeur"] = G[C][1]["valeur"] +1
         print("cellule contaminee: ", G[C][0])
 
-        #if nb_contaminee != 0
+
+
 
