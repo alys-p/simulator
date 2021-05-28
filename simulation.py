2e28bff342aae4c9249d0fb5d9e7a4e4a53601ce
alys-p <alysp@live.fr>
2021-05-28 20:02:18 +0200

    it works!!!!!

--- simulation.py
+++ simulation.py
@@ -4,5 +4,12 @@ from parametres import *
 from mise_a_jour import *
 
 G = configuration_initial(v, n, p)
+
+for i in range(t_maladie + 1):
+    print()
+    affichage.afficher_grille(G)
+    print()
+    print()
+    parcours_cell(G)
+
 affichage.afficher_grille(G)
-parcours_cell(G)
