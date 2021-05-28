63eef883c2954d255a735fbcb3f5df82981db9f0
ap-carrot <arianerolong@gmail.com>
2021-05-28 23:57:20 +0200

    all commented ;)

--- simulation.py
+++ simulation.py
@@ -2,14 +2,28 @@ import affichage
 from creation_config import *
 from parametres import *
 from mise_a_jour import *
+from time import sleep
 
-G = configuration_initial(v, n, p)
+def simulation():
+    S = {'decedees': 0, 'contaminees': 0, 'gueries': 0, }
+    G = configuration_initial(v, n, p)
+    for i in range(nb_tours):
+        print()
+        affichage.afficher_grille(G)
+        affichage.statistiques(G, S)
+        print()
+        print("cellules:")
+        print("decedées:", S['decedees'], "  contaminées:", S['contaminees'],
+            "  guerries:", S['gueries'], "  malades en ce moment:", S['malades'])
+        sleep(1)  # j'utilise sleep pour que l'affichage se fasse en decalé
+        print()
+        print()
+        parcours_cell(G)
 
-for i in range(nb_tours):       # boucle d'éxecution de la simulation en fonction du nombre de tour voulue.
-    print()
     affichage.afficher_grille(G)
+    affichage.statistiques(G, S)
     print()
-    print()
-    parcours_cell(G)
-
-affichage.afficher_grille(G)
+    print("cellules:")
+    print("decedées:", S['decedees'], "  contaminées:", S['contaminees'],
+          "  guerries:", S['gueries'], "  malades en ce moment:", S['malades'])
+    sleep(1)  # j'utilise sleep pour que l'affichage se fasse en decalé
